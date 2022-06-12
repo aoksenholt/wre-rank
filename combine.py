5a249671bd82772b086353c5d6d0e23636542302
Anders B. Øksenholt <anders.oksenholt@sparebank1.no>
2022-06-12 10:38:12 +0200

    skriv til en fil

--- combine.py
+++ combine.py
@@ -4,7 +4,7 @@ import sys
 import pandas as pd
 
 if len(sys.argv) != 4:
-	print("Angi Excel-fil som data skal leses fra.\nMå inneholde 3 ark; MEN, WOMEN og ETIMING. Hhv WRE-ranking for menn, damer og data fra eTiming.\nParameter 1: Excel-fil.\nParameter 2: første startnr damer.\nParameter 3: første startnr herre\n\nEks:\n./combine.py input.xlsx 101 201")
+	print("Angi Excel-fil som data skal leses fra.\nMå inneholde 3 ark; MEN, WOMEN og EVENTOR. Hhv WRE-ranking for menn, damer og data fra Eventor.\nParameter 1: Excel-fil.\nParameter 2: første startnr damer.\nParameter 3: første startnr herre\n\nEks:\n./combine.py input.xlsx 101 201")
 	sys.exit()
 
 innfil = sys.argv[1]
@@ -17,11 +17,11 @@ all_dfs = pd.read_excel(innfil, sheet_name=None)
 
 women_df = all_dfs['WOMEN']
 men_df = all_dfs['MEN']
-etime_df = all_dfs['ETIMING'].rename(columns = {'IOF-person-id':'IOF_ID'})
+etime_df = all_dfs['EVENTOR'].rename(columns = {'IOF-person-id':'IOF_ID'})
 
 all_df = pd.concat([women_df, men_df])
 
-all_df.to_excel('all.xlsx', index=False)
+#all_df.to_excel('all.xlsx', index=False)
 
 df = etime_df.merge(all_df[["IOF_ID","WRS_POINTS"]], on="IOF_ID", how="left")
 
@@ -45,14 +45,19 @@ print(len(d21e.index))
 
 #df = df.drop(columns = ['WRS_POINTS'])
 
-print("\n\nSkriver H 21-E til fil h21e.xlsx ...")
-h21e.to_excel('h21e.xlsx', index=False)
+merged = pd.concat([h21e, d21e]	)
+print("Skriver til fil WRE_Eventor_merged.xlsx...")
 
-print("Skriver D 21-E til fil d21e.xlsx ...")
-d21e.to_excel('d21e.xlsx', index=False)
+merged.to_excel('WRE_Eventor_merged.xlsx', index=False)
+
+#print("\n\nSkriver H 21-E til fil h21e.xlsx ...")
+#h21e.to_excel('h21e.xlsx', index=False)
+
+#print("Skriver D 21-E til fil d21e.xlsx ...")
+#d21e.to_excel('d21e.xlsx', index=False)
 
 # 
 # https://pbpython.com/pandas-excel-tabs.html
-# https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.concat.html
+#			 https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.concat.html
 
 
