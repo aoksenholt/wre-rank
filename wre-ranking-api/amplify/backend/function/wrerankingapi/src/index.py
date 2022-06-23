7e08446a9eaaa4c4d8129191224d30b07b95ac25
Anders B. Øksenholt <anders.oksenholt@sparebank1.no>
2022-06-23 13:17:41 +0200

    add post endpoint

--- wre-ranking-api/amplify/backend/function/wrerankingapi/src/index.py
+++ wre-ranking-api/amplify/backend/function/wrerankingapi/src/index.py
@@ -1,3 +1,4 @@
+from crypt import methods
 import awsgi
 
 from flask_cors import CORS
@@ -15,3 +16,15 @@ def handler(event, context):
 @app.route(BASE_ROUTE, methods=['GET'])
 def seed():
     return jsonify(message="hello seed")
+
+@app.route(BASE_ROUTE, methods=['POST'])
+def perform_merge():
+    request_json = request.get_json()
+
+    men_first_startno=request_json.get("men_first_startno")
+    women_first_startno=request_json.get("women_first_startno")
+
+    print("Først startnummer menn: " + str(men_first_startno))
+    print("Først startnummer damer: " + str(women_first_startno))
+
+    return jsonify(message="merge performed")
--- wre-ranking-api/src/index.js
+++ wre-ranking-api/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Amplify, API } from 'aws-amplify';
+import config from './aws-exports';
+
+
+Amplify.configure(config)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +16,7 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const getData = async () => {
+  const data = await API.get('wrerankingapi', '/seed')
+  console.log(data)
+}
